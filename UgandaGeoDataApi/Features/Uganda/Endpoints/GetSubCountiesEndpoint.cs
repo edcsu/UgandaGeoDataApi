68704e1f1134fd3b0c4f8370dcb9f03042f89a52
Ssewannonda Keith <38857594+edcsu@users.noreply.github.com>
2024-07-15 21:54:40 +0300

    feat: add sub counties endpoint

---
+++ UgandaGeoDataApi/Features/Uganda/Endpoints/GetSubCountiesEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using FastEndpoints;
+using UgandaGeoDataApi.Features.Uganda.Models;
+using UgandaGeoDataApi.Features.Uganda.Services;
+using UgandaGeoDataApi.Features.Uganda.ViewModels;
+
+namespace UgandaGeoDataApi.Features.Uganda.Endpoints
+{
+    public class GetSubCountiesEndpoint : Endpoint<SubCountySearchRequest, IEnumerable<SubCountyViewModel>>
+    {
+        public required JsonFileService FileService { get; set; }
+        
+        public override void Configure()
+        {
+            Get("/api/subcounties");
+            AllowAnonymous();
+            Description(b => b
+            .ProducesProblemDetails(400, "application/json+problem") //if using RFC errors 
+            .ProducesProblemFE<InternalErrorResponse>(500));
+        }
+
+        public override async Task HandleAsync(SubCountySearchRequest req, CancellationToken ct)
+        {
+            var subCounties = FileService.GetSubCounties();
+            
+            List<SubCountyViewModel> viewModels = [];
+            foreach (var subCounty in subCounties)
+            {
+                viewModels.Add(MapFromEntity(subCounty));
+            };
+
+            await SendAsync(viewModels, cancellation: ct);
+        }
+
+        public SubCountyViewModel MapFromEntity(SubCounty subCounty) => new()
+        {
+            Id = subCounty.Id,
+            Name = subCounty.Name,
+            County = subCounty.County,
+        };
+    }
+}
---
+++ UgandaGeoDataApi/Features/Uganda/ViewModels/SubCountyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace UgandaGeoDataApi.Features.Uganda.ViewModels
+{
+    public class SubCountyViewModel
+    {
+        public string Id { get; set; } = default!;
+
+        public string Name { get; set; } = default!;
+
+        public string County { get; set; } = default!;
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
